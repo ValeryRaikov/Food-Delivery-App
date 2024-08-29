ef1d4048459524cb5f5af35a346e0449c473137e
Valery Raikov <124359973+ValeryRaikov@users.noreply.github.com>
2024-08-29 10:27:39 +0300

    Setup Home page

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
+import ExploreMenu from './explore-menu/ExploreMenu';
+import Header from './header/Header';
+
 import './Home.css';
 
 export default function Home() {
+    const [category, setCategory] = useState('All');
+
     return (
         <div>
-
+            <Header />
+            <ExploreMenu category={category} setCategory={setCategory} />
         </div>
     );
 }
---
+++ frontend/src/components/home/header/Header.jsx
@@ -0,0 +1,13 @@
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="header-contents">
+                <h2>Order your favourite food here</h2>
+                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings.</p>
+                <button>view menu</button>
+            </div>
+        </div>
+    );
+}
